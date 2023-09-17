a7fc5942641090af3bd665484cefa8559e7995ab
BYREDDI RAMANA BABU <127107319+BYREDDI@users.noreply.github.com>
2023-09-17 18:43:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,6 +6,22 @@ const person = {
 };
 
 // Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
+let team=[];
+for(let i in players)
+	{
+		team.push(i);
+	}
+let team1=[];
+for(let i of players)
+	{
+		team1.push(i);
+	}
+let cap1={};
+cap1.name="John Doe";
+cap1.age=80;
+
+
+
 
 window.players = players;
 window.person = person;
